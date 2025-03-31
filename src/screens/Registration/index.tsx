3fcb6468f0a9396b0dc8c5e616c0f0f8e2af6b98
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2025-03-31 19:17:19 -0300

    Create registration screen

---
+++ src/screens/Registration/index.tsx
@@ -0,0 +1,75 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { 
+  Container, ContentContainer, ColumnContainer, RowContainer,
+  DietContainer, LabelDiet, ContainerSelect, SelectView, Circle, TextSelect 
+} from "./styles";
+import Title from "@components/Title";
+import Input from "@components/Input";
+import { ButtonFill } from "@components/Button";
+
+export default function Registration() {
+
+  return (
+    <Container>
+      <Title 
+        title="Nova refeição"
+      />
+
+      <ContentContainer>
+        <ColumnContainer>
+          <Input 
+            label="Nome"
+          />
+
+          <Input 
+            label="Descrição"
+            multiline
+            numberOfLines={4}
+            autoCorrect={false}
+            style={{height: 120, minHeight: 120, textAlignVertical: 'top'}}
+          />
+
+          <RowContainer>
+            <View style={{flex: 1}}>
+              <Input 
+                label="Data"
+              />
+            </View>
+
+            <View style={{flex: 1}}>
+              <Input 
+                label="Hora"
+              />
+            </View>
+          </RowContainer>
+
+          <DietContainer>
+            <LabelDiet>Está dentro da dieta?</LabelDiet>
+
+            <RowContainer>
+              <ContainerSelect>
+                <SelectView>
+                  <Circle variant="SUCCESS" />
+                  <TextSelect>Sim</TextSelect>
+                </SelectView>
+              </ContainerSelect>
+
+              <ContainerSelect>
+                <SelectView>
+                  <Circle variant="FAIL" />
+                  <TextSelect>Não</TextSelect>
+                </SelectView>
+              </ContainerSelect>
+            </RowContainer>
+          </DietContainer>
+        </ColumnContainer>
+
+        <ButtonFill 
+          text="Cadastrar refeição"
+          variant="FILL"
+        />
+      </ContentContainer>
+    </Container>
+  );
+}
---
+++ src/screens/Registration/styles.ts
@@ -0,0 +1,77 @@
+import styled, { DefaultTheme } from "styled-components/native";
+
+type SelectBoxProps = "SUCCESS" | "FAIL" | "NEUTRAL";
+type VariantProps = Exclude<SelectBoxProps, "NEUTRAL">;
+
+type Props = {
+  checked?: SelectBoxProps;
+  variant: VariantProps;
+}
+
+const Container = styled.View`
+  flex: 1;
+  padding-top: 56px;
+  background-color: ${({ theme }: { theme: DefaultTheme }) => theme.COLORS.GRAY_500 };
+`;
+
+const ContentContainer = styled.View`
+  flex: 1;
+  justify-content: space-between;
+  padding: 40px 24px;
+  background-color: ${({ theme }: { theme: DefaultTheme }) => theme.COLORS.GRAY_700 };
+  border-radius: 20px 20px 0 0;
+  box-shadow: 0 0 30px rgba(0, 0, 0, 0.5);
+`;  
+
+const ColumnContainer = styled.View`
+  flex-direction: column;
+  gap: 24px;
+`;
+
+const RowContainer = styled.View`
+  flex-direction: row;
+  gap: 20px;
+`;
+
+const DietContainer = styled.View``;
+
+const LabelDiet = styled.Text`
+  margin-bottom: 8px;
+  color: ${({ theme }: { theme: DefaultTheme }) => theme.COLORS.GRAY_200 };
+  font-family: ${({ theme }: { theme: DefaultTheme }) => theme.FONT_FAMILY.BOLD };
+  font-size: ${({ theme }: { theme: DefaultTheme }) => theme.FONT_SIZE.SM }px;
+`;
+
+const ContainerSelect = styled.TouchableHighlight`
+  padding: 16px;
+  flex: 1;
+  gap: 8px;
+  background-color: ${({ theme, checked }: { theme: DefaultTheme, checked: Props["checked"] }) => checked === "SUCCESS" ? theme.COLORS.GREEN_LIGHT : checked === "FAIL" ? theme.COLORS.RED_LIGHT : theme.COLORS.GRAY_600 };
+  border: 1px solid ${({ theme, checked }: { theme: DefaultTheme, checked: Props["checked"] }) => checked === "SUCCESS" ? theme.COLORS.GREEN_DARK : checked === "FAIL" ? theme.COLORS.RED_DARK : theme.COLORS.GRAY_600 };
+  border-radius: 6px;
+`;
+
+const SelectView = styled.View`
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+  justify-content: center;
+`;
+
+const Circle = styled.View`
+  width: 8px;
+  height: 8px;
+  background-color: ${({ theme, variant }: { theme: DefaultTheme, variant: Props["variant"] }) => variant === "SUCCESS" ? theme.COLORS.GREEN_DARK : theme.COLORS.RED_DARK };
+  border-radius: 4px;
+`;
+
+const TextSelect = styled.Text`
+  color: ${({ theme }: { theme: DefaultTheme }) => theme.COLORS.GRAY_100 };
+  font-family: ${({ theme }: { theme: DefaultTheme }) => theme.FONT_FAMILY.BOLD };
+  font-size: ${({ theme }: { theme: DefaultTheme }) => theme.FONT_SIZE.SM }px;
+`;
+
+export { 
+  Container, ContentContainer, ColumnContainer, RowContainer, 
+  DietContainer, LabelDiet, ContainerSelect, SelectView, Circle, TextSelect
+};
