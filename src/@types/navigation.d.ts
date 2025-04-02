443b12d66d1e56cc442801522ecb1596d8c73bdb
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2025-04-02 20:27:16 -0300

    create and config route to project

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,33 @@
+import { StatusProps } from "@components/Meal/styles";
+import { DietDatailsVariant } from "@screens/DietDetails/styles";
+
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Home: undefined;
+      DietDetails: {
+        percent: string;
+        variant: DietDatailsVariant;
+        sequence: number;
+        total_meal: number;
+        in_meal: number;
+        out_meal: number;
+      }
+      Registration: undefined;
+      InfoMeal: {
+        title: string;
+        name: string;
+        hour: string;
+        meal: string;
+        status: StatusProps;
+      },
+      Edition: {
+        title: string;
+        name: string;
+        hour: string;
+        meal: string;
+        status: StatusProps;
+      }
+    }
+  }
+}
