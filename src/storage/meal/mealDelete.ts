9679d7c6a0e38e280a36a8825cb6d9d3f810aa30
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2025-04-08 18:43:21 -0300

    Finish delete meal
    Its possibel remove one meal, and remove de section when there are'nt a meal

--- src/storage/meal/mealDelete.ts
+++ src/storage/meal/mealDelete.ts
@@ -12,18 +12,23 @@ export default async function mealDelete(title: string, hour: string){
     if(mealsData && mealsData.length > 0){
       newMealsData = mealsData.filter((meal) => meal.hour !== hour);
     }
+    
+    if(!mealsAll) return;
 
-    if(mealsAll && mealsAll.length > 0){
+    if(newMealsData.length > 0){
       const meals = mealsAll.map(meal => {
         if(meal.title.includes(title)){
           meal.data = [...newMealsData];
         }
         return meal;
-      })
+      });
 
-      await AsyncStorage.setItem(MEAL_COLLECTION, JSON.stringify(meals))
+      await AsyncStorage.setItem(MEAL_COLLECTION, JSON.stringify(meals));
+    } else {
+      const meals = mealsAll.filter(meal => meal.title !== title);
+      await AsyncStorage.setItem(MEAL_COLLECTION, JSON.stringify(meals));
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
