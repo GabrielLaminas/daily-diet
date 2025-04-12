38f117f215cc833a6d7596a99258cfcf9aeb5f0e
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2025-04-12 13:54:26 -0300

    Create get percent data

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -11,13 +11,35 @@ import { Meal, TitleMeal } from "@components/Meal";
 
 import { useNavigation } from "@react-navigation/native";
 import { mealGetAll } from "storage/meal/mealGetAll";
+import statisticCalculated from "storage/statistic/statisticCalculated";
+import statisticGetAll from "storage/statistic/statisticGetAll";
+
+type StateProps = "SUCCESS" | "FAIL" | "NEUTRAL";
 
 export default function Home() {
   const [meals, setMeals] = useState<DataMealDTO[]>([]);
+  const [titlePercent, setTitlePercent] = useState(0);
+  const [statePercent, setStatePercent] = useState<StateProps>("NEUTRAL");
   const navigation = useNavigation();
-  
-  function handleOpenPercentage(){
-    navigation.navigate("DietDetails", { percent: "90,86", variant: "SUCCESS", sequence: 22, total_meal: 20, in_meal: 99, out_meal: 10 });
+
+  async function handleOpenPercentage(){
+    try {
+      const statistics = await statisticGetAll();
+
+      if(statistics){
+        navigation.navigate("DietDetails", { 
+          percent: statistics.percent, 
+          variant: statistics.variant, 
+          sequence: statistics.sequence, 
+          total_meal: statistics.total_meal, 
+          in_meal: statistics.in_meal, 
+          out_meal: statistics.out_meal 
+        });
+      }
+      
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   function handleOpenNewMeal(){
@@ -35,18 +57,35 @@ export default function Home() {
     }
   }
 
+  async function calculateStatistic(){
+    try {
+      const mealStatistic = await statisticCalculated();
+      if(mealStatistic){
+        setTitlePercent(mealStatistic.percent);
+        setStatePercent(mealStatistic.variant);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   function handleOpenInfoMeal({ title, meal }: { title: string, meal: DataInfoDTO }){
     navigation.navigate("InfoMeal", { title: title, id: meal.id, name: meal.name, description: meal.description, hour: meal.hour, status: meal.status });
   }
 
   useEffect(() => {
+    calculateStatistic();
     getAllMeals();
   }, []);
 
   return (
     <Container>
       <Header />
-      <Percent onPress={handleOpenPercentage} />
+      <Percent 
+        title={titlePercent} 
+        state={statePercent} 
+        onPress={handleOpenPercentage} 
+      />
       <NewMeal onPress={handleOpenNewMeal} />
 
       <SectionList 
